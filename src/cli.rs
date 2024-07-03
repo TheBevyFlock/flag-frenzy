3dddff214cd5d3b1d9702a609ea55440217bd707
BD103 <59022059+BD103@users.noreply.github.com>
2024-07-02 21:27:21 -0400

    feat: use `cargo-locate-project` for default `Cargo.toml` location

--- src/cli.rs
+++ src/cli.rs
@@ -1,11 +1,12 @@
 use argh::FromArgs;
-use std::path::PathBuf;
+use serde::Deserialize;
+use std::{io, path::PathBuf, process::{Command, Stdio}};
 
 /// Automatically checks combinations of feature flags for a Cargo project.
 #[derive(FromArgs, Debug)]
 pub struct CLI {
     /// the path to `Cargo.toml`
-    #[argh(option, default = "PathBuf::from(\"Cargo.toml\")")]
+    #[argh(option, default = "locate_manifest().expect(\"Failed to find Cargo.toml.\")")]
     pub manifest_path: PathBuf,
 
     /// package(s) to check
@@ -15,4 +16,27 @@ pub struct CLI {
     /// the path to the config folder
     #[argh(option)]
     pub config: Option<PathBuf>,
+}
+
+/// Represents the output of `cargo-locate-project`.
+#[derive(Deserialize, Debug)]
+struct ProjectLocation {
+    pub root: PathBuf,
+}
+
+/// Returns the path of the current project's `Cargo.toml` file, using `cargo-locate-project`.
+fn locate_manifest() -> io::Result<PathBuf> {
+    let output = Command::new("cargo")
+        .stderr(Stdio::inherit()) // Print errors directly to terminal.
+        .arg("locate-project")
+        .args(["--message-format", "json"]) // Output JSON format.
+        .args(["--color", "never"]) // Do not output ANSI escape codes.
+        .output()?;
+
+    if output.status.success() {
+        let location: ProjectLocation = serde_json::from_slice(&output.stdout).map_err(io::Error::other)?;
+        Ok(location.root)
+    } else {
+        Err(io::Error::other("`cargo-locate-project` exited with a non-zero exit code."))
+    }
 }
