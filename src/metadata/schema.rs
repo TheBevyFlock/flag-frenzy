00e0df2719e5d2269ba78affd0bade62d85c4d3f
BD103 <59022059+BD103@users.noreply.github.com>
2024-07-12 16:12:53 -0400

    feat: skip optional dependencies during internment

--- src/metadata/schema.rs
+++ src/metadata/schema.rs
@@ -17,17 +17,5 @@ pub struct Package {
     /// The name of the crate.
     pub name: String,
     /// A list of all features in a crate.
-    #[serde(deserialize_with = "deserialize_feature_map_into_vec")]
-    pub features: Vec<String>,
-}
-
-/// Deserializes a map [`HashMap<String, Vec<String>>`] into a [`Vec<String>`] of its keys.
-///
-/// Used by [`Package::features`] to discard the feature dependencies.
-fn deserialize_feature_map_into_vec<'de, D>(deserializer: D) -> Result<Vec<String>, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    let result = <HashMap<String, Vec<String>>>::deserialize(deserializer)?;
-    Ok(result.into_keys().collect())
+    pub features: HashMap<String, Vec<String>>,
 }
