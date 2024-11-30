740973185859cfe797a0871b42e43e325dea028b
Shachar Langbeheim <nihohit@gmail.com>
2024-11-30 19:33:38 +0200

    Filter combos that contain dependencies. (#21)
    If a combo contains two features that one is a direct or indirect
    dependency, the combo will now be filtered.
    
    https://github.com/TheBevyFlock/flag-frenzy/issues/19

--- src/combos/features.rs
+++ src/combos/features.rs
@@ -38,4 +38,67 @@ pub fn feature_combos<'a>(
         })
         // Only yield combinations that pass all rules for this crate.
         .filter(move |combo| rules.iter().all(|r| r.validate(combo)))
+        // Only yield combinations that don't contain dependencies.
+        .filter(move |combo| {
+            combo.iter().all(|key| {
+                combo
+                    .iter()
+                    .all(|other_key| !storage.is_dependency(*key, *other_key))
+            })
+        })
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    use crate::{config::WorkspaceConfig, intern::intern_features};
+
+    use super::feature_combos;
+
+    #[test]
+    fn test_filter_dependencies() {
+        let mut features_map = HashMap::new();
+        features_map.insert("foo".to_string(), Vec::new());
+        features_map.insert("bar".to_string(), vec!["foo".to_string()]);
+        features_map.insert("foobar".to_string(), vec!["bar".to_string()]);
+        features_map.insert("unrelated".to_string(), Vec::new());
+
+        let schema_config = crate::config::schema::Config {
+            max_combo_size: Some(3),
+            skip_optional_deps: None,
+            rules: vec![],
+        };
+        let workspace_config = WorkspaceConfig::new(HashMap::new(), schema_config);
+        let storage = intern_features(features_map, workspace_config.get(""));
+
+        let mut combos: Vec<_> = feature_combos(&storage, workspace_config.get("foo"))
+            .map(|combo| {
+                let mut vec = combo
+                    .iter()
+                    .map(|key| storage.get(*key).unwrap())
+                    .collect::<Vec<_>>();
+                vec.sort();
+                vec
+            })
+            .collect();
+        combos.sort();
+
+        let mut expected = vec![
+            vec![],
+            vec!["foo"],
+            vec!["bar"],
+            vec!["unrelated"],
+            vec!["foobar"],
+            vec!["unrelated", "foo"],
+            vec!["unrelated", "bar"],
+            vec!["unrelated", "foobar"],
+        ];
+        for combo in &mut expected {
+            combo.sort();
+        }
+        expected.sort();
+
+        assert_eq!(combos, expected);
+    }
 }
